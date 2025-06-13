a0f34f1cb8a38be275b9a697cb2b64f3c9536c4f
NurMohammadakram <nurmohammad19k8@gmail.com>
2025-06-13 09:33:11 +0600

    higher order catchAsync function added to avoid try catch repeatation

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,46 +1,32 @@
-import { NextFunction, Request, Response } from 'express';
 import { studentServices } from './student.service';
-import sendResponse from '../../middlewares/utils/sendResponse';
+import sendResponse from '../../utils/sendResponse';
+import catchAsync from '../../utils/catchAsync';
 
-const getAllStudent = async (req: Request, res: Response, next: NextFunction) => {
-  try {
+const getAllStudent = catchAsync(async (req, res) => {
     const result = await studentServices.getAllStudentFromDB();
     sendResponse(res, {message: 'Successfully retrieved all student data', data: result})
-  } catch (error) {
-    next(error)
-  }
-};
+  
+});
 
-const getSingleStudent = async (req: Request, res: Response, next: NextFunction) => {
-  try {
+const getSingleStudent = catchAsync(async (req, res) => {
     const { studentId: id } = req.params;
     const result = await studentServices.getSingleStudentFromDB(id as string);
     sendResponse(res, {message: 'successfully retrieved the user.', data: result})
-  } catch (error) {
-    next(error)
-  }
-};
+  
+});
 
-const updateStudent = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const { studentId: id } = req.params;
-    const data = req.body;
-    const updateResult = await studentServices.updateStudentIntoDB(id, data);
-    sendResponse(res, {message: 'successfully updated user', data: updateResult})
-  } catch (error) {
-    next(error)
-  }
-};
+const updateStudent = catchAsync(async (req, res) => {
+  const { studentId: id } = req.params;
+  const data = req.body;
+  const updateResult = await studentServices.updateStudentIntoDB(id, data);
+  sendResponse(res, { message: 'successfully updated user', data: updateResult });
+});
 
-const deleteStudent = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const { studentId: id } = req.params;
-    const result = await studentServices.deleteStudentFromDB(id);
-    sendResponse(res, {message: 'Your profile is deleted!', data : result})
-  } catch (error) {
-    next(error)
-  }
-};
+const deleteStudent = catchAsync(async (req, res) => {
+  const { studentId: id } = req.params;
+  const result = await studentServices.deleteStudentFromDB(id);
+  sendResponse(res, { message: 'Your profile is deleted!', data: result });
+});
 
 export const studentControllers = {
   deleteStudent,
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,20 +1,12 @@
-import { NextFunction, Request, Response } from 'express';
 import { userServices } from './user.service';
-import sendResponse from '../../middlewares/utils/sendResponse';
+import sendResponse from '../../utils/sendResponse';
+import catchAsync from '../../utils/catchAsync';
 
-const createStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
+const createStudent = catchAsync(async (req, res) => {
     const { password, student: studentData } = req.body;
     const result = await userServices.createStudentIntoDB(password, studentData);
     sendResponse(res, {message: 'student created successfully',  data: result})
-  } catch (error) {
-    next(error);
-  }
-};
+});
 
 export const userControllers = {
   createStudent,
