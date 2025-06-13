d28cb7235f7a23cd813becd9615ff40193b545ee
NurMohammadakram <nurmohammad19k8@gmail.com>
2025-06-14 03:44:53 +0600

    catchAsync implemented instead of try catch in validation

--- src/app/middlewares/validateRequest.ts
+++ src/app/middlewares/validateRequest.ts
@@ -1,15 +1,25 @@
 import { NextFunction, Request, Response } from "express";
 import { AnyZodObject } from "zod";
+import catchAsync from "../utils/catchAsync";
 
+/*
 const validationRequest = (schema: AnyZodObject) => {
 return async (req: Request, res: Response, next: NextFunction) => {
     try {
-    await schema.parse( {body: req.body});
+    await schema.parseAsync( {body: req.body});
     next();
     }catch (error) {
         next(error)
     }
 }
 }
+*/
+const validationRequest = (schema: AnyZodObject) => {
+    return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+        Promise.resolve(schema.parseAsync({ body: req.body }))
+            .then(() => next())
+            .catch(error => next(error));
+    })
+}   
 
 export default validationRequest;
