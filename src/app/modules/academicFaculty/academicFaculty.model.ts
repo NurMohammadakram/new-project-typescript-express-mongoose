e593efa49828273ff5a2e50bc44cb7559175fb6d
NurMohammadakram <nurmohammad19k8@gmail.com>
2025-09-05 23:24:25 +0600

    academic faculty pre filter checking added

--- src/app/modules/academicFaculty/academicFaculty.model.ts
+++ src/app/modules/academicFaculty/academicFaculty.model.ts
@@ -2,10 +2,34 @@ import { model, Schema } from "mongoose";
 import { IAcademicFaculty } from "./academicFaculty.interface";
 
 const academicFacultySchema = new Schema<IAcademicFaculty> ({
-    name: { type: String, required: true, unique: true }
+    name: { 
+        type: String, 
+        required: true, 
+        unique: true }
 },
 {
     timestamps: true,
+}
+)
+
+academicFacultySchema.pre('save', async function (next) {
+    const isFacultyExists = await AcademicFacultyModel.findOne({
+        name: this.name
+    })
+    if (isFacultyExists) {
+        throw new Error('This faculty already exists!');
+    }
+    next();
+}
+)
+
+academicFacultySchema.pre('findOneAndUpdate', async function (next) {
+    const isFacultyExists = await AcademicFacultyModel.findOne(this.getQuery())
+    
+    if (!isFacultyExists) {
+        throw new Error('Faculty not found!');
+    }
+    next();
 }
 )
 
